86c24524e66477a1e725d90b6f17718d0cbf6bc3
Leandro Guindani Gehlen <leandrogehlen@gmail.com>
2016-12-15 16:40:58 -0200

    Allows to define initial selected item in plugins based on `select`

--- SelectWidget.php
+++ SelectWidget.php
@@ -3,6 +3,7 @@
 namespace soluto\kendoui;
 
 use yii\helpers\Html;
+use yii\helpers\Json;
 
 class SelectWidget extends InputWidget
 {
@@ -13,16 +14,42 @@ class SelectWidget extends InputWidget
     public $items = [];
 
     /**
+     * @var string the initial selected element
+     */
+    public $selected = [];
+
+    /**
      * @inheritdoc
      */
     public function run()
     {
+        $options = $this->options;
+
         if ($this->hasModel()) {
-            echo Html::activeDropDownList($this->model, $this->attribute, $this->items, $this->options);
+            echo Html::activeDropDownList($this->model, $this->attribute, $this->items, $options);
         } else {
-            echo Html::dropDownList($this->name, $this->value, $this->items, $this->options);
+            echo Html::dropDownList($this->name, $this->value, $this->items, $options);
         }
 
-        $this->registerPlugin($this->options['id']);
+        $value = isset($options['value']) ? $options['value'] : null;
+        if ($value === null) {
+            $value = $this->hasModel() ? Html::getAttributeValue($this->model, $this->attribute) : $this->value;
+        }
+
+        $id = $options['id'];
+        $this->registerPlugin($id);
+
+        $plugin = "$('#$id').data('$this->pluginName')";
+        $view = $this->getView();
+
+        if (!empty($this->selected) && array_filter(array_values($this->selected))) {
+            $selected = Json::encode($this->selected);
+            $view->registerJs("$plugin.dataSource.add($selected);");
+        }
+
+        if ($value !== null) {
+            $view->registerJs("$plugin.value($value);");
+            $view->registerJs("$plugin.trigger('change');");
+        }
     }
 }
