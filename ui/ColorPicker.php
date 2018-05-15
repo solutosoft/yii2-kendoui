6ff9fd97c5afa689a693460b6db7032d84344987
Diogo Biolo D`Agostini <diogobd@gmail.com>
2018-05-15 20:36:16 -0300

    Added `Color` plugins

---
+++ ui/ColorPicker.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace soluto\kendoui\ui;
+
+use soluto\kendoui\base\InputWidget;
+
+class ColorPicker extends InputWidget
+{
+    /**
+     * @inheritdoc
+     */
+    public $pluginName = 'kendoColorPicker';
+}
